3d69791b6346b9ef8d9d231161c649cba0f28113
bulgerxie <541898607@qq.com>
2017-05-19 07:27:51 +0000

    database debug

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -1,10 +1,23 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     //
+    protected $table = 'articles';
+
+    protected $fillable = [
+        'title',
+        'abstract',
+        'content',
+        'article_img',
+        'author_name',
+        'author_id',
+        'be_dz',
+        'be_pl',
+        'be_see'
+    ];
 }
